e141447310c3ebb16a584a68b31bb6296765d99d
salvas <131319897+salvasCODE@users.noreply.github.com>
2026-03-19 12:38:09 -0500

    Add new calendar data entries

--- assets/js/data/calendarData.js
+++ assets/js/data/calendarData.js
@@ -7,5 +7,21 @@ export const calendarData = [
         startDate: "20 Abril, 2026",
         modality: "Presencial",
         closingSoon: true
+    },
+    {
+        id: 2,
+        program: "Nivelación en Matemática Básica y Cálculo I",
+        closingDate: "06 Febrero, 2026",
+        startDate: "09 Febrero, 2026",
+        modality: "Presencial - sede Chilca",
+        closingSoon: true
+    },
+    {
+        id: 3,
+        program: "OPEN DAY 2026: Facultad de Ciencias Matemáticas Viva y Sostenible",
+        closingDate: "10 Febrero, 2026",
+        startDate: "13 Febrero, 2026",
+        modality: "Presencial",
+        closingSoon: true
     }
 ];
